a6c6c2c9994c3756ba355a84e000ba88616a2f94
carefreepapa <64464846+carefreepapa@users.noreply.github.com>
2022-12-24 01:30:05 -0500

    Changes to be committed: modified:   install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,12 @@
 print("installing computercraft scripts")
+
+local git_repo = "https://raw.githubusercontent.com/carefreepapa/computercraft/main"
+local files = {
+    "constants.lua",
+    "vector.lua",
+    "turtle.lua",
+}
+
+for i = 1, #files do
+    shell.run("wget", git_repo .. files[i])
+end
